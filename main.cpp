20a74520d0bfddf39d66da7676184ca6ff451e53
Ryan Hartlage <ryanplusplus@gmail.com>
2016-11-04 12:41:15 -0400

    Added make-based build

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "mbed.h"
+
+DigitalOut myled(LED1);
+
+int main() {
+    while(1) {
+        myled = 1;
+        wait(0.8);
+        myled = 0;
+        wait(0.2);
+        
+        myled = 1;
+        wait(0.2);
+        myled = 0;
+        wait(0.2);
+    }
+}
